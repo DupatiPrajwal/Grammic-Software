042f05135b6da81af153e5935bda24b33b0aa27d
Dupati Prajwal <dupatiprajwal@gmail.com>
2026-08-19 22:11:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+print("Welcome to Grammic!")
+print("Your writing assistant is starting...")
+
+# TEXT :
+text = input("enter or paste your text :")
+print(text)
+
+# ANALYZING TEXT :
+print("================================")
+print("GRAMMIC ANALYSIS")
+print("================================")
+characters = len(text)
+print(characters)
+words = text.split()
+print(len(words))
+count = 0
+for i in text :
+    if i == "?" or i == "!" or i == "." :
+        count+=1 
+        print(count)
+paragraphs = text.split("\n\n")
+paragraphs_count = len(paragraphs)
+print(paragraphs_count)
 
