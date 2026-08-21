d7e6cae639a4c70004dbb0dbf8909466b359e2c1
Dupati Prajwal <dupatiprajwal@gmail.com>
2026-08-21 21:39:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 print("Welcome to Grammic!")
 print("Your writing assistant is starting...")
 
-# TEXT :
+# TEXT & #ANALYZING TEXT :    
 text = input("enter or paste your text :")
-print(text)
+if text.strip() == "" :
+    print("Error")
+else :
+    print("===========ANALYZING THE TEXT===========")
+    print(text)
+    characters = len(text)
+    print("Characters In Your Text:",characters)
+    words = text.split()
+    print("Words In Your Text :",len(words))
+    count = 0
+    for i in text :
+        if i == "?" or i == "!" or i == "." :
+            count+=1 
+    print("Punctuations In Your Text :",count)    
+    paragraphs = text.split("\n\n")
+    paragraphs_count = len(paragraphs)
+    print("Paragrahs In Your Text:",paragraphs_count)
 
-# ANALYZING TEXT :
-print("================================")
-print("GRAMMIC ANALYSIS")
-print("================================")
-characters = len(text)
-print(characters)
-words = text.split()
-print(len(words))
-count = 0
-for i in text :
-    if i == "?" or i == "!" or i == "." :
-        count+=1 
-        print(count)
-paragraphs = text.split("\n\n")
-paragraphs_count = len(paragraphs)
-print(paragraphs_count)
 
